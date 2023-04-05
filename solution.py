ec4c4b6ee5bda25fca74a666603da6b341199f6b
Булат Куандыков <112857500+BulatKZ@users.noreply.github.com>
2023-04-05 22:00:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,8 +11,8 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    loc = np.mean(x)
+    #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     left = chi2.ppf(1 - alpha/2, df=len(x))
     right = chi2.ppf(alpha/2, df=len(x))
     y = 0
